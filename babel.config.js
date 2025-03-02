954442aeb57a35b0e725e2295b031c12c0d98829
vedatdogann <vedat202dogan@gmail.com>
2025-03-02 15:07:17 +0300

    hata düzeltildi

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
+/* module.exports = {
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module:react-native-dotenv",
+      {
+        moduleName: "@env",
+        path: ".env",
+        allowUndefined: false,
+      },
+    ],
+    ['@babel/plugin-transform-private-methods'],
+  ],
+}; */
+
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
   plugins: [
@@ -9,5 +24,9 @@ module.exports = {
         allowUndefined: false,
       },
     ],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
   ],
 };
+
