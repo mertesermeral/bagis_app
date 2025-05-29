e7dc4e7806ac088feb6162f8aefa3d8559386216
Mert Eser Meral <eser.meral@ogr.sakarya.edu.tr>
2025-05-30 02:54:52 +0300

    chore: update package versions and dependencies in package.json
    - Changed version of @react-native-picker/picker from ^2.9.0 to 2.9.0
    - Added expo-dev-client with version ~5.0.20
    - Updated expo-modules-core to version ~2.2.3
    - Updated react-native from 0.76.7 to 0.76.9
    - Changed version of react-native-pager-view from ^6.5.1 to 6.5.1
    - Updated react-native-reanimated to version 3.18.0
    - Changed version of react-native-webview from ^13.13.4 to 13.12.5
    - Added react-native-gesture-handler with version 2.9.0

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,10 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: ['babel-preset-expo'],
   plugins: [
-    [
-      "module:react-native-dotenv",
-      {
-        moduleName: "@env",
-        path: ".env",
-        allowUndefined: false,
-      },
-    ],
+    ['module:react-native-dotenv', { moduleName: '@env', path: '.env', allowUndefined: false }],
     ['@babel/plugin-transform-class-properties', { loose: true }],
     ['@babel/plugin-transform-private-methods', { loose: true }],
     ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    'react-native-reanimated/plugin',          // <-- en sonda
   ],
 };
-
